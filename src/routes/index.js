48c1f98ea96a339e22737f62ee0c4ca26cd7fa0a
Syed Mohsin Raza <mohsin.raza@10pearls.com>
2022-09-02 12:31:35 +0500

    Minor change

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = new express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   res.send('Welcome to Stock Trades API');
 });
 
